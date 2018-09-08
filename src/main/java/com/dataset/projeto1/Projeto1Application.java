c50b52d77d77aa6d18a6588a6ad9d0f6d0588cf9
Iogo <=>
2018-09-08 00:51:09 -0300

    Criando os produtos e associacoes

--- src/main/java/com/dataset/projeto1/Projeto1Application.java
+++ src/main/java/com/dataset/projeto1/Projeto1Application.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dataset.projeto1.domain.Categoria;
+import com.dataset.projeto1.domain.Produto;
 import com.dataset.projeto1.repositories.CategoriaRepository;
+import com.dataset.projeto1.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class Projeto1Application implements CommandLineRunner{
@@ -16,6 +18,9 @@ public class Projeto1Application implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto1Application.class, args);
 	}
@@ -26,6 +31,18 @@ public class Projeto1Application implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null,"Informatica");
 		Categoria cat2 = new Categoria(null,"Deposito");
 		
+		Produto p1 = new Produto(null,"Computador",2000);
+		Produto p2 = new Produto(null,"Impressora",800);
+		Produto p3 = new Produto(null,"Mouse",80);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.save(Arrays.asList(cat1,cat2));
+		produtoRepository.save(Arrays.asList(p1,p2,p3));
 	}
 }
--- src/main/java/com/dataset/projeto1/domain/Categoria.java
+++ src/main/java/com/dataset/projeto1/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.dataset.projeto1.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -15,6 +18,9 @@ private static final long serialVersionUID = 1L;
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String nome;
+  
+  @ManyToMany(mappedBy = "categorias")
+  private List<Produto> produtos = new ArrayList<>();
   
   public Categoria(){
   }
@@ -40,6 +46,16 @@ public String getNome() {
 public void setNome(String nome) {
 	this.nome = nome;
 }
+
+public List<Produto> getProdutos() {
+	return produtos;
+}
+
+public void setProdutos(List<Produto> produtos) {
+	this.produtos = produtos;
+}
+  
+
 
 @Override
 public String toString() {
@@ -70,6 +86,6 @@ public boolean equals(Object obj) {
 		return false;
 	return true;
 }
-  
+
   
 }
