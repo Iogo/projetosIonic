e0e2233984d293a19306daf3983a80633180f270
Iogo <=>
2018-09-08 01:33:25 -0300

    Tratamento de erros

---
+++ src/main/java/com/dataset/projeto1/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.dataset.projeto1.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.dataset.projeto1.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardErro> objectNotFound(ObjectNotFoundException e,HttpServletRequest request){
+		StandardErro err = new StandardErro(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
--- src/main/java/com/dataset/projeto1/services/CategoriaService.java
+++ src/main/java/com/dataset/projeto1/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.dataset.projeto1.domain.Categoria;
 import com.dataset.projeto1.repositories.CategoriaRepository;
+import com.dataset.projeto1.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,17 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if(obj == null) {
+			
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id
+					+",Tipo: "+Categoria.class.getName());
+		}
+		
 		return obj;
 	}
+	
+	
+	
+	
 }
