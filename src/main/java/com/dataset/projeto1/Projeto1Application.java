c9ac360eac7a8461e71a27360952f75071b3edf1
Iogo <=>
2018-09-08 00:08:38 -0300

    comitando

--- src/main/java/com/dataset/projeto1/Projeto1Application.java
+++ src/main/java/com/dataset/projeto1/Projeto1Application.java
@@ -1,12 +1,31 @@
 package com.dataset.projeto1;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Projeto1Application {
+import com.dataset.projeto1.domain.Categoria;
+import com.dataset.projeto1.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class Projeto1Application implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto1Application.class, args);
+	}
+	
+	
+	@Override
+	public void run(String... args)throws Exception{
+		Categoria cat1 = new Categoria(null,"Informatica");
+		Categoria cat2 = new Categoria(null,"Deposito");
+		
+		categoriaRepository.save(Arrays.asList(cat1,cat2));
 	}
 }
